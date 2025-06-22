16502e1559ad43894069ac3243656e0bf96df964
Wendel Lopes <wendelopes@msn.com>
2025-06-22 05:33:13 -0300

    import gin

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+}
